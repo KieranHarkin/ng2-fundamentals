6751af7f9467c9d2390622e65c06bb70368d4433
Kieran Harkin <kieranharkin@googlemail.com>
2017-06-16 13:43:20 +0100

    Using data from the tempalte

--- app/app.module.ts
+++ app/app.module.ts
@@ -16,23 +16,18 @@ import {
 
 import { appRoutes } from './routes';
 import { EventsAppComponent } from './events-app.component';
-import {  } from './events/events-list.component';
-import {  } from './events/event-thumbnail.component';
-import {  } from './events/create-event.component';
 import { NavBarComponent } from './nav/navbar.component';
-import {  } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
-import {  } from './events/event-details/event-details.component';
 import { Error404Component } from './errors/404.component';
-import {  } from './events/event-details/event-route-activator.service';
-import {  } from './events/events-list-resolver.service';
+import { AuthService } from './user/auth.service';
+
 
 
 @NgModule({
     imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
     declarations: [EventsAppComponent, EventsListComponent, 
                     EventThumbnailComponent, EventDetailsComponent, CreateEventComponent, NavBarComponent, Error404Component],
-    providers: [EventService, ToastrService, EventRouterActivator, EventListResolver,
+    providers: [EventService, ToastrService, EventRouterActivator, EventListResolver, AuthService,
     {provide: 'canDeactivateCreateEvent', useValue: checkDirtyState}
     ],
     bootstrap: [EventsAppComponent]
