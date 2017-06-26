9db1dcdb4a02385319fabf3bc1b8dc56587d5413
KieranHarkin <kieranharkin@googlemail.com>
2017-06-26 01:15:36 +0100

    Setting up Karma and tslint

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpModule } from "@angular/http";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { EventsListNavbarComponent } from './nav/events-list-navbar.component';
+import { EventsListNavbarComponent } from "./nav/events-list-navbar.component";
 
-import { 
+import {
     EventsListComponent,
     EventThumbnailComponent,
     EventService,
@@ -19,7 +19,7 @@ import {
     VoterService,
     LocationValidator,
     DurationPipe
-} from './events/index';
+} from "./events/index";
 
 import { 
     TOASTR_TOKEN,
@@ -27,14 +27,14 @@ import {
     JQUERY_TOKEN,
     CollapsibleWellComponent,
     SimpleModalComponent,
-    ModalTriggerDirective
- } from './common/index';
+    ModalTriggerDirective,
+ } from "./common/index";
 
-import { appRoutes } from './routes';
-import { EventsAppComponent } from './events-app.component';
-import { NavBarComponent } from './nav/navbar.component';
-import { Error404Component } from './errors/404.component';
-import { AuthService } from './user/auth.service';
+import { appRoutes } from "./routes";
+import { EventsAppComponent } from "./events-app.component";
+import { NavBarComponent } from "./nav/navbar.component";
+import { Error404Component } from "./errors/404.component";
+import { AuthService } from "./user/auth.service";
 
 declare let toastr: Toastr;
 declare let jQuery: Object;
@@ -42,12 +42,12 @@ declare let jQuery: Object;
 @NgModule({
     imports: [BrowserModule, FormsModule, HttpModule, ReactiveFormsModule, RouterModule.forRoot(appRoutes)],
     declarations: [
-        EventsAppComponent, 
-        EventsListComponent, 
-        EventThumbnailComponent, 
-        EventDetailsComponent, 
-        CreateEventComponent, 
-        NavBarComponent, 
+        EventsAppComponent,
+        EventsListComponent,
+        EventThumbnailComponent,
+        EventDetailsComponent,
+        CreateEventComponent,
+        NavBarComponent,
         EventsListNavbarComponent,
         Error404Component,
         CreateSessionComponent,
@@ -60,22 +60,22 @@ declare let jQuery: Object;
         DurationPipe
     ],
     providers: [
-        EventService, 
-        {provide: TOASTR_TOKEN, useValue: toastr}, 
-        {provide: JQUERY_TOKEN, useValue: jQuery}, 
-        EventResolver, 
-        EventListResolver, 
+        EventService,
+        {provide: TOASTR_TOKEN, useValue: toastr},
+        {provide: JQUERY_TOKEN, useValue: jQuery},
+        EventResolver,
+        EventListResolver,
         AuthService,
-        {provide: 'canDeactivateCreateEvent', useValue: checkDirtyState},
+        {provide: "canDeactivateCreateEvent", useValue: checkDirtyState},
         VoterService
     ],
-    bootstrap: [EventsAppComponent]
+    bootstrap: [EventsAppComponent],
 })
 export class AppModule {
 
 }
 
-function checkDirtyState(componnet:CreateEventComponent) {
+function checkDirtyState(componnet: CreateEventComponent) {
     if (componnet.isDirty) {
         return window.confirm("You have not save this event, do you really want to cancel?");
     }
